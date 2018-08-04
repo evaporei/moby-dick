09bd4a7355b73ad16c9d686d52d2be4456be4058
Otávio Pace <otaviopp8@gmail.com>
2018-08-03 23:16:04 -0300

    child: make process enviroment isolated

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+#![feature(libc)]
+
 use std::env;
 use std::process::Command;
 use std::process;
+use std::fs;
+
+extern crate libc;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -42,8 +47,68 @@ fn child(args: &Vec<String>) {
 
     let program_args = &args[3..];
 
+    // unsafe {
+    //     libc::mount(
+    //         "rootfs".as_ptr() as *const i8,
+    //         "rootfs".as_ptr() as *const i8,
+    //         "".as_ptr() as *const i8,
+    //         0x1000,
+    //         "".as_ptr() as *const libc::c_void
+    //     );
+    //     libc::mkdir("rootfs/oldrootfs".as_ptr() as *const i8, 700);
+    //     libc::syscall(libc::SYS_pivot_root, "rootfs".as_ptr(), "rootfs/oldrootfs".as_ptr());
+    //     libc::chdir("/".as_ptr() as *const i8);
+    // }
+
+    Command::new("/bin/mkdir")
+        .arg("-p")
+        .arg("/home/rootfs")
+        .status()
+        .unwrap();
+
+    Command::new("/bin/chroot")
+        .arg("/home/rootfs")
+        .status()
+        .unwrap();
+
+    Command::new("/bin/chdir")
+        .arg("/home/rootfs")
+        .status()
+        .unwrap();
+
+    unsafe {
+        libc::chdir("/".as_ptr() as *const i8);
+    }
+
+    Command::new("/bin/mount")
+        // .arg("/home/rootfs")
+        // .arg("/")
+        .arg("--source")
+        .arg("proc")
+        .arg("--target")
+        .arg("proc")
+        .arg("--types")
+        .arg("proc")
+        .status()
+        .unwrap();
+
+    //
+    // fs::create_dir_all("/home/rootfs").unwrap();
+    // unsafe {
+    //     libc::chroot("/home/rootfs".as_ptr() as *const i8);
+    //     libc::chdir("/".as_ptr() as *const i8);
+    //     libc::mount(
+    //         "proc".as_ptr() as *const i8,
+    //         "proc".as_ptr() as *const i8,
+    //         "proc".as_ptr() as *const i8,
+    //         0,
+    //         "".as_ptr() as *const libc::c_void
+    //     );
+    // }
+
     Command::new(program_name)
         .args(program_args)
+        .current_dir("/")
         .status()
         .expect("Problem running program");
 }
